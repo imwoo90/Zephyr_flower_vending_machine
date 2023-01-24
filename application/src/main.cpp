8149cd1f51ce4236e043089f6e963a83d00a20f9
imwoo90 <wim90@kakao.com>
2023-01-24 15:55:48 +0900

    main.cpp

---
+++ application/src/main.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2022 Lukasz Majewski, DENX Software Engineering GmbH
+ * Copyright (c) 2019 Peter Bigot Consulting, LLC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* Sample which uses the filesystem API with littlefs */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main);
+
+#define PARTITION_NODE DT_NODELABEL(lfs1)
+FS_FSTAB_DECLARE_ENTRY(PARTITION_NODE);
+struct fs_mount_t *mp = &FS_FSTAB_ENTRY(PARTITION_NODE);
+
+// static char test_buf[40*1024] = "wrtie sucess";
+
+int main(void)
+{
+	int fd;
+	k_msleep(5000);
+
+	while(1) {
+		LOG_INF("Hello main cpp");
+		k_msleep(1000);
+	}
+
+
+	// LOG_INF("test start");
+	// k_msleep(1000);
+	// while(1) {
+	// 	fd = open("/root/test.txt", O_CREAT|O_WRONLY);
+	// 	if (fd < 0){
+	// 		LOG_INF("file open fail %d", fd);
+	// 		break;
+	// 	}
+
+	// 	write(fd, test_buf, sizeof(test_buf));
+	// 	close(fd);
+
+	// 	char buf[30] = {0,};
+	// 	fd = open("/root/test.txt", O_RDONLY);
+	// 	read(fd, buf, 12);
+	// 	LOG_INF("%s", buf);
+	// 	close(fd);
+	// 	k_msleep(1);
+	// }
+	return 0;
+}
